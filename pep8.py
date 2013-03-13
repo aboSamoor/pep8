3df040d51d95bb382845412e9752abf01cd0cb83
Rami Al-Rfou' <rmyeid@gmail.com>
2013-03-13 15:24:23 -0300

    Change 4 spaces to 2 spaces indentation
    Motivation:
    I really do not like 4 spaces indentation.

--- pep8.py
+++ pep8.py
@@ -359,7 +359,7 @@ def missing_whitespace(logical_line):
 def indentation(logical_line, previous_logical, indent_char,
                 indent_level, previous_indent_level):
     r"""
-    Use 4 spaces per indentation level.
+    Use 2 spaces per indentation level.
 
     For really old code that you don't want to mess up, you can continue to
     use 8-space tabs.
@@ -374,7 +374,7 @@ def indentation(logical_line, previous_logical, indent_char,
     Okay: a = 1\nb = 2
     E113: a = 1\n    b = 2
     """
-    if indent_char == ' ' and indent_level % 4:
+    if indent_char == ' ' and indent_level % 2:
         yield 0, "E111 indentation is not a multiple of four"
     indent_expect = previous_logical.endswith(':')
     if indent_expect and indent_level <= previous_indent_level:
